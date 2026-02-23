adc22a669183e17a4a0c9dd774bbac0b01c20101
Ondrej Babinský <babinskyondrej@gmail.com>
2026-02-23 02:10:44 +0100

    backend init

---
+++ GtIntel.Api/Program.cs
@@ -0,0 +1,33 @@
+
+namespace GtIntel.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+
+
+            // Swagger only in Development
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
